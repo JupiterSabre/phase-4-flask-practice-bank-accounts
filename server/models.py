a3a2e6d8989b3f67912b09d2a6640bba66e0b562
Zulai Romero <zulairomero@gmail.com>
2023-09-18 14:43:49 -0400

    Completed models and validatons

--- server/models.py
+++ server/models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -10,3 +11,56 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 # write your models here!
+
+class Bank(db.Model, SerializerMixin):
+    __tablename__ = "banks"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+
+    accounts = db.relationship("Account", back_populates="bank")
+    customers = association_proxy("accounts", "customers")
+
+    serialize_rules = ("-accounts.bank")
+
+
+
+
+class Account(db.Model, SerializerMixin):
+    __tablename__ = "accounts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    balance = db.Column(db.Integer, nullable=False)
+    account_type = db.Column(db.String, nullable=False)
+
+    bank_id = db.Column(db.Integer, db.ForeignKey("banks.id"))
+    customer_id = db.Column(db.Integer,db.ForeignKey("customers.id"))
+
+    bank = db.relationship("Bank", back_populates="accounts")
+    customers = db.relationship("Customer", back_populates="accounts")
+
+    serialize_rules = ("-bank.accounts", "customer.accounts")
+
+
+    @validates("balance")
+    def check_balance(self, key, number):
+        if number < 0:
+            raise ValueError("Balance cannot be negative")
+        return number
+    
+
+
+
+class Customer(db.Model, SerializerMixin):
+    __tablename__ = "customers"
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+
+    accounts = db.relationship("Account", back_populates="customers")
+    banks = association_proxy("accounts", "banks")
+
+    serialize_rules = ("-accounts.customer")
+
+
+    
+
--- server/seed.py
+++ server/seed.py
@@ -1,15 +1,62 @@
 #!/usr/bin/env python3
 
 from app import app
-from models import db # models go here
+from models import db, Account, Customer, Bank # models go here
 from faker import Faker
+import random
 
 faker = Faker()
 
+def create_accounts():
+    accounts = []
+    balance = [34, 52, 654, 34, 234, 334, 24, 111, 911, 653, 782, 834]
+    account_type = ["Checking", "Savings"]
+    for _ in range(25):
+        account = Account(balance= random.choice(balance),  account_type=random.choice(account_type), bank=random.choice(banks), customers= random.choice(customers))
+        accounts.append(account)
+    return accounts
+
+def create_banks():
+    banks = []
+    bank_name = ["Chase", "Washington Mutual", "Bank of America", "Capital One", "Charles Schwab"]
+    for _ in range(25):
+        bank = Bank(name=random.choice(bank_name))
+        banks.append(bank)
+    return banks
+
+
+
+
+def create_customers():
+    customers = []
+    fname = ["Bob", "Kirsten", "Marco", "Enrique", "John", "Parker", "Zulai", "James", "Harold", "Liev", "Josie", "Carol", "Jamie", "Jonathan", "Susan", "Becky", "Rosa", "Rebecca", "Josephine"]
+    lname = ["Smith", "Davila", "Einstein", "Lewis", "Chen", "Lee", "Romero", "Pretlow", "Washington", "Davila"]
+    for _ in range (25):
+        customer = Customer(first_name=random.choice(fname), last_name=random.choice(lname))
+        customers.append(customer)
+    return customers
+
+
 if __name__ == '__main__':
     with app.app_context():
+        print("Clearing db...")
+        Account.query.delete()
+        Bank.query.delete()
+        Customer.query.delete()
         print("Seeding database...")
 
-        # write your seeds here!
+        customers = create_customers()
+        db.session.add_all(customers)
+        db.session.commit()
+
+        banks = create_banks()
+        db.session.add_all(banks)
+        db.session.commit()
+
+        accounts = create_accounts()
+        db.session.add_all(accounts)
+        db.session.commit()
+
+
 
         print("Seeding complete!")
